4d02cfa6d4b8671a01c6cd9a8ecea36ea471c79c
Erick Kei Igarashi Alves <erick.igarashi05@gmail.com>
2024-08-26 09:43:15 -0300

    disciplina.py
    26/08

---
+++ disciplina.py
@@ -0,0 +1,6 @@
+class Disciplina:
+    def __init__ (self, nome, professor, carga_horaria, sala):
+        self.nome = nome
+        self.professor = professor
+        self.carga_horaria = carga_horaria
+        self.sala = sala
