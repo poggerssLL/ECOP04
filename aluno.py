5aad16596f1c8ac7aa429dec29ad0eff2c3385cb
Erick Kei Igarashi Alves <erick.igarashi05@gmail.com>
2024-08-26 09:42:03 -0300

    aluno.py
    26/08 ECOP

--- aluno.py
+++ aluno.py
@@ -1,7 +1,25 @@
 from classes import Aluno
-aluno = Aluno("2024004143", "Erick", "ECA",["ECOP"])
-aluno.insere_disciplina("Objeto fodase")
-print(aluno.disciplinas)
-aluno.remove_disciplina("Objeto fodase")
-print(aluno.disciplinas)
+from disciplina import Disciplina
+disciplinas_disponiveis = []
+
+disc = Disciplina("POO", "Alyson", 64, "LEC1")
+disciplinas_disponiveis.append(disc)
+
+disc = Disciplina("Calculo", "EULER", 32, "I2118")
+disciplinas_disponiveis.append(disc) 
+
+aluno = Aluno("2024004143", "Erick", "ECA",["POO"], disciplinas_disponiveis)
+aluno2 = Aluno("2024001234", "JUAO", "ECA",["POO"], disciplinas_disponiveis)
+
+if aluno.insere_disciplina("Calculo",disciplinas_disponiveis):
+    print("Aluno matriculado")
+else:
+    print("Aluno nao matriculado")
+
+
+##print(aluno.disciplinas)
+##aluno.remove_disciplina("Objeto fodase")
+for x in range(len(aluno.disciplinas)):
+    print(aluno.disciplinas[x].nome)
+
 aluno.imprime_aluno()
