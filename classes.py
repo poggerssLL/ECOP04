3e584b3c53089c203164b9117a7ba681cabb800d
Erick Kei Igarashi Alves <erick.igarashi05@gmail.com>
2024-08-26 09:42:33 -0300

    classes.py
    26/08

--- classes.py
+++ classes.py
@@ -1,15 +1,32 @@
+
 class Aluno:
-    def __init__(self, matricula, nome, curso, disciplinas):
+    
+    def __init__(self, matricula, nome, curso, disciplinas, disciplinas_disponiveis):
         self.matricula = matricula
         self.nome = nome
         self.curso = curso
-        self.disciplinas = disciplinas
-    def insere_disciplina(self, disciplina):        
-        self.disciplinas.append(disciplina)
-    def remove_disciplina(self, disciplina):
-        self.disciplinas.remove(disciplina)
+        self.disciplinas = []
+        for disc in disciplinas:
+            for disc_disp in disciplinas_disponiveis:
+                if disc == disc_disp.nome:
+                    self.disciplinas.append(disc_disp)
+                    print("Aluno {} matriculado em {}".format(self.nome, disc))
+                    break
+                else:
+                    print("Aluno {} NAO matriculado em {}". format(self.nome, disc))
+    def insere_disciplina(self, disciplina, disciplinas_disponiveis):        
+        if disciplina in disciplinas_disponiveis:
+            self.disciplinas.append(disciplina)
+            return True
+        else: 
+           return False
+
+    
+
     def imprime_aluno(self):
         print("Nome: {}".format(self.nome))
         print("Matricula: {}".format(self.matricula))
         print("Curso: {}".format(self.curso))
-        print("Disciplinas: {}".format(self.disciplinas))
+        for x in range (len(self.disciplinas)):
+
+            print(self.disciplinas[x].nome)
