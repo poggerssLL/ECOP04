161398a369264e6daef3ab6b5fdcab6b213af82a
Erick Kei Igarashi Alves <erick.igarashi05@gmail.com>
2025-05-26 17:17:21 -0300

    Create tempcontrol.h

---
+++ 260525 ADC temperatura/tempcontrol.h
@@ -0,0 +1,9 @@
+#ifndef tempcontrol_h
+
+#define tempcontrol_h
+
+void ConfigurarLimiteSuperior(unsigned char temp);
+void ConfigurarLimiteInferior(unsigned char temp);
+void AtualizarSistema(void);
+#endif
+
